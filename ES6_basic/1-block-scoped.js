cae419053017eb634d729e48daeecf028a87b1b7
ertix222 <ertix222@gmail.com>
2025-03-24 14:16:43 +0100

    t1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,5 +2,13 @@ export default function taskBlock() {
   const task = false;
   const task2 = true;
 
-  return [task, task2];
+  if (trueOrFalse) {
+    // eslint-disable-next-line
+    const task = true;
+    // eslint-disable-next-line
+    const task2 = false;
+  }
+  
+    return [task, task2];
 }
+  
