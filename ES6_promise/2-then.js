9b65a00e66f8d200129ea06f4b9b02fb01aba7ca
ertix222 <ertix222@gmail.com>
2025-04-04 09:14:34 +0200

    t2

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'Success' };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error('The fake API is not working currently');
+    });
+}
+
+export default handleResponseFromAPI;
