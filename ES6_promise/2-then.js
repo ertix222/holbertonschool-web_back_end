34d8432f7c9ab5e0e5a98df83da33e866c0a0bc6
ertix222 <ertix222@gmail.com>
2025-04-04 09:20:36 +0200

    change console log in a finally

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,12 +1,13 @@
 function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
-      console.log('Got a response from the API');
       return { status: 200, body: 'Success' };
     })
     .catch(() => {
-      console.log('Got a response from the API');
-      return new Error('The fake API is not working currently');
+      return new Error();
+    })
+    .finally(() => {
+      console.log('Got a response from the API')
     });
 }
 
