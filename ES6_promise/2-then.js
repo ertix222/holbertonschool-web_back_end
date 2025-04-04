5ec73933209d983922447d616b5700564ed20e7b
ertix222 <ertix222@gmail.com>
2025-04-04 09:22:59 +0200

    change console log in a finally

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -7,7 +7,7 @@ function handleResponseFromAPI(promise) {
       return new Error();
     })
     .finally(() => {
-      console.log('Got a response from the API')
+      console.log('Got a response from the API');
     });
 }
 
