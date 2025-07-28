f20d1465819412b80dafaa5532f3eb0772162048
ertix222 <ertix222@gmail.com>
2025-07-28 10:42:39 +0200

    fix docstring

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""this module define a function to add two float numbers
+"""This module define a function to add two float numbers.
 """
 
 def add(a: float, b: float) -> float:
-    """return the sum of two float numbers
+    """Return the sum of two float numbers.
     """
     return a + b
