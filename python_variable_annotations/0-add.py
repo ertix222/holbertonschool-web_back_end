c8d358421893e39975e1f0d3907d50fdeb5e36d1
ertix222 <ertix222@gmail.com>
2025-07-28 10:34:03 +0200

    basic annotation add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this module define a function to add two float numbers
+"""
+
+def add(a: float, b: float) -> float:
+    """return the sum of two float numbers
+    """
+    return a + b
