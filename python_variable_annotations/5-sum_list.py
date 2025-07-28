203fe939452d56a3690f7c9828d1110a0e255ef6
ertix222 <ertix222@gmail.com>
2025-07-28 13:55:52 +0200

    add the improt of List

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,8 +2,9 @@
 """This module defines a function that sums a list of float numbers.
 """
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Return the sum of all floats elements in the list.
     """
     return sum(input_list)
