069b5738fac9a632fd0444bdbae68eead19e481a
ertix222 <ertix222@gmail.com>
2025-07-30 11:47:09 +0200

    the basics of async

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Module that defines an asynchronous coroutine to wait a random time.
+"""
+
+import asyncio
+import random
+from typing import Union
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Waits for a random delay between 0 and the max_delay secondes.
+    """
+    delay: float = random.uniform (0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
