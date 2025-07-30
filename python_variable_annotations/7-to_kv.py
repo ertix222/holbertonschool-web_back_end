4edd9a1189ae716c7d478e68c346e1eeb0bb56a6
ertix222 <ertix222@gmail.com>
2025-07-30 09:21:53 +0200

    complex types string and int/float to tuples

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-"""This module defines a function that sums a list of integers and floats.
+"""This module defines a function that returns a tuple
+    with a string and the square of a number.
 """
 
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return a tuple where the first element is the string 'k'
     """
-    return (k, float(v ** 2))
+    return (k, v ** 2)
