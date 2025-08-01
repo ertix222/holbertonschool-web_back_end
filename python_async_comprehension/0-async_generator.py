bad06dd4ff607c95dd225a176f0616daa9f0dea8
ertix222 <ertix222@gmail.com>
2025-08-01 14:18:32 +0200

    Async Generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Async generator module.
+"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """Yields random numbers asynchronously.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
