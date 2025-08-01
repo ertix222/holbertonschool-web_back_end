2d7403d24528a3d2de94936b22a3c6e9c99ef230
ertix222 <ertix222@gmail.com>
2025-08-01 15:07:37 +0200

    pycodestyle fix

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Module to asynchronously collect random numbers using async comprehension.
 """
-import asyncio
+
 import random
+import asyncio
 import typing
 async_generator = __import__('0-async_generator').async_generator
 
