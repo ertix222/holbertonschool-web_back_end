a5fa5342ae34a8e22e8157e51d806f3524f8d0dc
ertix222 <ertix222@gmail.com>
2025-08-22 15:08:02 +0200

    implement HolbertonCourse class with type validation and getters/setters

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this.name = name;
-    this.length = length;
-    this.students = students;
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
   get name() {
@@ -20,20 +20,26 @@ export default class HolbertonCourse {
     return this._length;
   }
 
-  set length(value) {
-    if (typeof value !== 'number' || !Number.isFinite(value) || !Number.isInteger(value)) {
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    this._length = value;
+    this._length = newLength;
   }
 
   get students() {
     return this._students;
   }
-  set students(value) {
-    if (!Array.isArray(value) || !value.every((s) => typeof s === 'string')) {
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
       throw new TypeError('Students must be an array of strings');
     }
-    this._students = value;
+    for (const student of newStudents) {
+      if (typeof student !== 'string') {
+        throw new TypeError('Students must be an array of strings');
+      }
+    }
+    this._students = newStudents;
   }
 }
