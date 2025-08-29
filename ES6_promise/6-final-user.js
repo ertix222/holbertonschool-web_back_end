8da7c1ab9f56ad86b13892b65044b48cca922567
ertix222 <ertix222@gmail.com>
2025-08-29 23:59:15 +0200

    handle profile signup with allSettled to manage multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,9 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]);
+  if (typeof firstName !== 'string' || typeof lastName !== 'string') {
+    return ('firstname and lastname must be a string');
+  }
+  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
 }
