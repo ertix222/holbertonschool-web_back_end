ddfcd7721fa3c6610d42fe410ff198d7161a0ada
ertix222 <ertix222@gmail.com>
2025-09-10 10:42:56 +0200

    Create a basic HTTP server

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const host = 'localhost';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, {'content-type': 'text/plain'});
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
+
+module.exports = app;
